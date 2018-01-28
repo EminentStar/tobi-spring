6da93f71e432667157ab26b172b07d03e0ea7fbb
sixmonthq <wnsrb3843@naver.com>
2018-01-29 08:34:52 +0900

    2.2 UserDaoTest 개선. 2.2.1 테스트 검증의 자동화

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -12,7 +12,7 @@ public class UserDaoTest {
     // classpath를 시작하는 / 는 넣을 수도 있고 생략할 수도 있음.
     // 시작하는 / 가 없는 경우 항상 루트에서부터 시작하는 classpath라는 점을 기억.
     ApplicationContext context =
-    new GenericXmlApplicationContext("applicationContext.xml");
+      new GenericXmlApplicationContext("applicationContext.xml");
     UserDao dao = context.getBean("userDao", UserDao.class);
 
     String id = "junk3843";
@@ -29,9 +29,13 @@ public class UserDaoTest {
     System.out.println(user.getId() + " 등록 성공");
 
     User user2 = dao.get(user.getId());
-    System.out.println(user2.getName());
-    System.out.println(user2.getPassword());
 
-    System.out.println(user2.getId() + " 조회 성공 ");
+    if (!user.getName().equals(user2.getName())) {
+      System.out.println("테스트 실패 (name)");
+    }else if (!user.getPassword().equals(user2.getPassword())) {
+      System.out.println("테스트 실패 (password)");
+    }else {
+      System.out.println("조회 테스트 성공");
+    }
   }
 }
