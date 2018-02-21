9c78b093caab810a3218204e0d79c40de3b25f9a
sixmonthq <wnsrb3843@naver.com>
2018-02-21 10:05:33 +0900

    5.2.3 트랜잭션 동기화: Connection 파라미터 제거/트랜잭션 동기화 적용

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,6 +1,5 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
 import java.util.List;
 
 import springbook.user.domain.User;
@@ -9,11 +8,11 @@ import springbook.user.domain.User;
  * UserDao의 인터페이스와 구현을 분리함으로써 데이터 액세스의 구체적인 기술과 UserDao의 클라이언트 사이에 DI가 적용됨.
  */
 public interface UserDao {
-  void add(Connection c, User user);
-  User get(Connection c, String id);
-  List<User> getAll(Connection c);
-  void deleteAll(Connection c);
-  void delete(Connection c, String id);
-  int getCount(Connection c);
-  void update(Connection c, User user);
+  void add(User user);
+  User get(String id);
+  List<User> getAll();
+  void deleteAll();
+  void delete(String id);
+  int getCount();
+  void update(User user);
 }
--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -3,6 +3,11 @@ package springbook.user.service;
 import java.sql.Connection;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
@@ -11,13 +16,21 @@ public class UserService {
   public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
   public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
-  UserDao userDao;
+  private UserDao userDao;
+  private DataSource dataSource;
 
   /**
    * UserDao 오브젝트의 DI가 가능하도록 setter method 추가
    */
   public void setUserDao(UserDao userDao) {
     this.userDao = userDao;
+  }
+
+  /**
+   * Connection을 생성할 때 사용할 DataSource를 DI받도록 함.
+   */
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   public void add(User user) {
@@ -28,15 +41,32 @@ public class UserService {
     userDao.add(user);
   }
 
-  public void upgradeLevels() {
-    Connection c = null;
-    List<User> users = userDao.getAll(c);
+  public void upgradeLevels() throws Exception {
+    TransactionSynchronizationManager.initSynchronization(); // 트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화
+    // DB 커넥션을 생성하고 트랜잭션을 시작. 이후의 DAO 작업을 모두 여기서 시작한 트랜잭션 안에서 진행됨
+    Connection c = DataSourceUtils.getConnection(dataSource); // DB 커넥션 생성과 동기화를 함께 해주는 유틸리티 메소드
+    c.setAutoCommit(false);
 
-    for (User user : users) {
-      if (canUpgradeLevel(user)) {
-        upgradeLevel(c, user);
+    try {
+      List<User> users = userDao.getAll();
+
+      for (User user : users) {
+        if (canUpgradeLevel(user)) {
+          upgradeLevel(user); // 트랜잭션 동기화가 되어 있는 채로 JdbcTemplate을 사용하면 JdbcTemplate의 작업에서 동기화시킨 DB 커넥션을 사용하게 됨. TODO: 이것도 learning test로 확인해볼 수 있지 않을까?
+        }
       }
+
+      c.commit(); // 정상적으로 작업을 마치면 transaction commit
+    } catch (Exception e) {
+      c.rollback(); // 예외가 발생하면 rollback
+      throw e;
+    } finally {
+      DataSourceUtils.releaseConnection(c, dataSource); // 스프링 유틸리티 메소드를 이용해 DB 커넥션을 안전하게 닫음.
+      // 동기화 작업 종료 및 정리
+      TransactionSynchronizationManager.unbindResource(this.dataSource);
+      TransactionSynchronizationManager.clearSynchronization();
     }
+
   }
 
   private boolean canUpgradeLevel(User user) {
@@ -57,9 +87,9 @@ public class UserService {
    * 테스트를 위한 상속을 할 때 private 접근제한이 걸려있어 오버라이딩이 불가.
    * 이번은 예제 상의 예외상황으로 상속을 위해 `protected` Access Specifier를 사용하자.
    */
-  protected void upgradeLevel(Connection c, User user) {
+  protected void upgradeLevel(User user) {
     user.upgradeLevel();
-    userDao.update(c, user);
+    userDao.update(user);
   }
 
 }
