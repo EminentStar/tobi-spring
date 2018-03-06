da1edda3865d48f17c87fe7318616c68b0427b3a
sixmonthq <wnsrb3843@naver.com>
2018-03-06 21:30:51 +0900

    6.3.2. 다이내믹 프록시(다이내믹 프록시 적용)

--- src/springbook/proxy/HelloProxyTest.java
+++ src/springbook/proxy/HelloProxyTest.java
@@ -3,6 +3,8 @@ package springbook.proxy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 public class HelloProxyTest {
@@ -16,7 +18,12 @@ public class HelloProxyTest {
 
   @Test
   public void helloUppercaseProxy() {
-    Hello proxiedHello = new HelloUppercase(new HelloTarget());
+    // 생성된 다이내믹 프록시 오브젝트는 Hello 인터페이스를 구현하고 잇으므로 Hello 타입으로 캐스팅해도 안전함.
+    Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+      getClass().getClassLoader(), // 동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더
+      new Class[] {Hello.class}, // 구현할 인터페이스
+      new UppercaseHandler(new HelloTarget()) // 부가기능과 위임 코드를 담은 InvocationHandler
+    );
     assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
     assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
     assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
