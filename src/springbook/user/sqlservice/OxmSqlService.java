e425c1af8a570b448d0c5886c903fad8ddaf348e
sixmonthq <wnsrb3843@naver.com>
2018-03-25 09:26:50 +0900

    7.3.2. OXM 서비스 추상화 적용: 멤버 클래스를 참조하는 통합 클래스

---
+++ src/springbook/user/sqlservice/OxmSqlService.java
@@ -0,0 +1,95 @@
+package springbook.user.sqlservice;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.springframework.oxm.Unmarshaller;
+
+import springbook.user.dao.UserDao;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+public class OxmSqlService implements SqlService {
+  /**
+   * final이브로 변경 불가능. OxmSqlServiec와 OxmSqlReader는 강하게 결합돼서 하나의 빈으로 등록되고 한 번에 설정할 수 있음.
+   *
+   * 이렇게 두 개의 클래스(OxmSqlService - OxmSqlReader)를 강하게 결합해서 확장이나 변경을 제한하는 이유는
+   * OXM을 이용하는 서비스 구조로 최적화하기 위해서임.
+   */
+  private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+
+  /**
+   * oxmSqlReader와 달리 단지 디폴트 오브젝트로 만들어진 프로퍼티. 따라서 필요에 따라 DI를 통해 교체 가능.
+   */
+  private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+  public void setSqlRegistry(SqlRegistry sqlRegistry) {
+    this.sqlRegistry = sqlRegistry;
+  }
+
+  /**
+   * # setUnmarshaller(), setSqlmapFile()
+   *
+   * OxmSqlReader의 공개된 프로퍼티를 통해 DI 받은 것을 그대로 멤버 클래스의 오브젝트에 전달.
+   * 이 setter들은 단일 빈 설정구조를 위한 창구 역할을 할 뿐임.
+   */
+  public void setUnmarsaller(Unmarshaller unmarsaller) {
+    this.oxmSqlReader.setUnmarshaller(unmarsaller);
+  }
+
+  public void setSqlmapFile(String sqlmapFile) {
+    this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+  }
+
+  @PostConstruct
+  public void loadSql() {
+    this.oxmSqlReader.read(this.sqlRegistry);
+  }
+
+  @Override
+  public String getSql(String key) throws SqlRetrievalFailureException {
+    try {
+      return this.sqlRegistry.findSql(key);
+    } catch (SqlNotFoundException e) {
+      throw new SqlRetrievalFailureException(e.getMessage());
+    }
+  }
+
+  /**
+   * private 멤버 클래스로 정의. top level class인 OxmSqlService만이 사용가능.
+   */
+  private class OxmSqlReader implements SqlReader {
+    private final static String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+
+    private Unmarshaller unmarshaller;
+    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+      this.unmarshaller = unmarshaller;
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+      this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+
+      try {
+        Source source = new StreamSource(
+          UserDao.class.getResourceAsStream(this.sqlmapFile));
+        Sqlmap sqlmap = (Sqlmap)this.unmarshaller.unmarshal(source);
+
+        for (SqlType sql : sqlmap.getSql()) {
+          sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+        }
+      } catch (IOException e) {
+        throw new IllegalArgumentException(this.sqlmapFile + "을 가져올 수 없습니다.", e);
+      }
+    }
+  }
+
+}
