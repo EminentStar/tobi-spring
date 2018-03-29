731a49398bac675296151c450cd3c554a010c307
sixmonthq <wnsrb3843@naver.com>
2018-03-29 09:08:30 +0900

    7.5.3.트랜잭션 적용: 다중 SQL 수정에 대한 트랜잭션 테스트

--- src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -1,11 +1,17 @@
 package springbook.user.sqlservice.updatable;
 
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
+import static org.junit.Assert.fail;
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
+import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
@@ -27,5 +33,24 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
   @After
   public void tearDown() {
     db.shutdown();
+  }
+
+  /**
+   * 트랜잭션이 적용됬는지 테스트
+   */
+  @Test
+  public void transactionalUpdate() {
+    checkFindResult("SQL1", "SQL2", "SQL3");
+
+    Map<String, String> sqlmap = new HashMap<String, String>();
+    sqlmap.put("KEY1", "Modified1");
+    sqlmap.put("KEY9999!@#$", "Modified9999"); // 존재하지 않는 키로 지정. 이로 인해 롤백이 일어나는지 확인할 수 있음.
+
+    try {
+      sqlRegistry.updateSql(sqlmap);
+      fail();
+    } catch (SqlUpdateFailureException e) {
+    }
+    checkFindResult("SQL1", "SQL2", "SQL3"); // 롤백됬는지 확인
   }
 }
