f68ab062badbcfb3bfe37d4ef1e85302743e232c
sixmonthq <wnsrb3843@naver.com>
2018-04-06 16:06:27 +0900

    중첩클래스를 이용한 프로파일 적용

--- src/springbook/conf/AppContext.java
+++ src/springbook/conf/AppContext.java
@@ -7,13 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springbook.user.dao.UserDao;
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceTest;
 
 /**
  * @Configuration: DI 정보로 사용될 자바 클래스로 지정
@@ -30,10 +36,8 @@ import springbook.user.dao.UserDao;
   }))
 @Import({
   DataBaseConfig.class,
-  SqlServiceContext.class,
-  TestAppContext.class,
-  ProductionAppContext.class
-})
+  SqlServiceContext.class
+}) // 스태틱 중첩클래스로 넣은 @Configuration 클래스는 스프링이 자동으로 포함해줌.
 public class AppContext {
   @Autowired
   private UserDao userDao;
@@ -76,4 +80,28 @@ public class AppContext {
     return transactionManager;
   }
 
+  @Configuration
+  @Profile("production")
+  public static class ProductionAppContext {
+    @Bean
+    public MailSender mailSender() {
+      JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+      mailSender.setHost("localhost");
+      return mailSender;
+    }
+  }
+
+  @Configuration
+  @Profile("test")
+  public static class TestAppContext {
+    @Bean
+    public UserService testUserService() {
+      return new UserServiceTest.TestUserService();
+    }
+
+    @Bean
+    public MailSender mailSender() {
+      return new DummyMailSender();
+    }
+  }
 }
